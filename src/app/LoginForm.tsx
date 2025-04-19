33159ef6131b3a13c5fd9d260b608ffb030701b5
Kaneska <117206537+Kaneska@users.noreply.github.com>
2025-04-19 22:58:39 +0530

    Update LoginForm.tsx

--- src/app/LoginForm.tsx
+++ src/app/LoginForm.tsx
@@ -1,5 +1,5 @@
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
